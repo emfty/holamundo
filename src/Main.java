fe53993e83d3d1feaf0821ba28eb9b5c019014c7
ESTUDIANTE <julianvivas544@gmail.com>
2022-09-20 16:55:15 -0500

    a

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    private final static double PI = 3.14;
+    private static int numero1;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite su numero pussi");
+        numero1 = input.nextInt();
+        System.out.println("Resultado: " + (numero1 *numero1));
+
+
+
+
+    }
+
+}
