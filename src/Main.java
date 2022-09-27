756267e7362587d2865078c40ccba4f651007c8a
emfty <106672676+emfty@users.noreply.github.com>
2022-09-27 17:54:10 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,70 @@
 import java.util.Scanner;
 
 public class Main {
-    private final static double PI = 3.14;
-    private static int numero1;
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Digite su numero pussi");
-        numero1 = input.nextInt();
-        System.out.println("Resultado: " + (numero1 *numero1));
+        Scanner Sc = new Scanner(System.in);
+        String nombre;
+        int  dias;
+        int sueldo;
+        String nombre2;
+        int  dias2;
+        int sueldo2;
+        String nombre3;
+        int  dias3;
+        int sueldo3;
+        String nombre4;
+        int  dias4;
+        int sueldo4;
+        String nombre5;
+        int  dias5;
+        int sueldo5;
+        sueldo = 800000;
+        double horas;
+        System.out.println("hola buenas trabajador digita tu nombre");
+        nombre = Sc.nextLine();
+        System.out.println("hola buenas trabajador digita tu edad");
+        dias = Sc.nextInt();
+        System.out.println("hola buenas trabajador digita horas trabajadas");
+        horas = Sc.nextByte();
+        System.out.println("su nombre es " +nombre+ " bien y su edad es" +dias+ " y sus horas son" +horas+ "");
+        System.out.println("y su sueldo es " +sueldo+ "gracias por consultar el siguiente ");
 
+        System.out.println("hola buenas trabajador digita tu nombre");
+        nombre2 = Sc.nextLine();
+        System.out.println("hola buenas trabajador digita tu edad");
+        dias2 = Sc.nextInt();
+        System.out.println("hola buenas trabajador digita horas trabajadas");
+        sueldo2 = Sc.nextByte();
+        System.out.println("su nombre es " +nombre2+ " bien y su edad es" +dias2+ " y sus horas son" );
+        System.out.println("y su sueldo es " +sueldo2+ "gracias por consultar el siguiente ");
+
+        System.out.println("hola buenas trabajador digita tu nombre");
+        nombre3 = Sc.nextLine();
+        System.out.println("hola buenas trabajador digita tu edad");
+        dias3 = Sc.nextInt();
+        System.out.println("hola buenas trabajador digita horas trabajadas");
+        sueldo3 = Sc.nextByte();
+        System.out.println("su nombre es " +nombre3+ " bien y su edad es" +dias3+ " y sus horas son" );
+        System.out.println("y su sueldo es " +sueldo3+ "gracias por consultar el siguiente ");
+
+        System.out.println("hola buenas trabajador digita tu nombre");
+        nombre4 = Sc.nextLine();
+        System.out.println("hola buenas trabajador digita tu edad");
+        dias4 = Sc.nextInt();
+        System.out.println("hola buenas trabajador digita horas trabajadas");
+        sueldo4 = Sc.nextByte();
+        System.out.println("su nombre es " +nombre4+ " bien y su edad es" +dias4+ " y sus horas son" );
+        System.out.println("y su sueldo es " +sueldo4+ "gracias por consultar el siguiente ");
+
+        System.out.println("hola buenas trabajador digita tu nombre");
+        nombre5 = Sc.nextLine();
+        System.out.println("hola buenas trabajador digita tu edad");
+        dias5 = Sc.nextInt();
+        System.out.println("hola buenas trabajador digita horas trabajadas");
+        sueldo5 = Sc.nextByte();
+        System.out.println("su nombre es " +nombre5+ " bien y su edad es" +dias5+ " y sus horas son" );
+        System.out.println("y su sueldo es " +sueldo5+ "gracias por consultar el siguiente ");
 
 
 
